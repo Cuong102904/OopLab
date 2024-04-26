a53094dcafe94cbf8d3a3de75d08cee64ba3efa9
Cuong <hoangmanhcuong1029@gmail.com>
2024-04-26 20:58:34 +0700

    update

---
+++ OtherProjects/hust/soict/globalict/lab3/NoGarbage.java
@@ -0,0 +1,28 @@
+package OtherProjects.hust.soict.globalict.lab3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+    public static void main(String[] args) {
+        String filename = "OtherProjects/hust/soict/globalict/lab3/test.txt";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        startTime = System.currentTimeMillis();
+        StringBuilder outputSB = new StringBuilder();
+        for (byte b : inputBytes) {
+            outputSB.append((char) b);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time taken by StringBuilder: " + (endTime - startTime) + "ms");
+    }
+
+}
