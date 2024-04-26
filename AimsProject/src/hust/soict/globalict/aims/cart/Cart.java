440d2d755e93749bc59adc2dfd12da2b35dd9135
Cuong <hoangmanhcuong1029@gmail.com>
2024-04-26 14:54:35 +0700

    Update the Cart class and CartTest class

--- AimsProject/src/hust/soict/globalict/aims/cart/Cart.java
+++ AimsProject/src/hust/soict/globalict/aims/cart/Cart.java
@@ -1,4 +1,5 @@
 package hust.soict.globalict.aims.cart;
+
 import hust.soict.globalict.aims.disc.DigitalVideoDisc;
 
 public class Cart {
@@ -65,11 +66,48 @@ public class Cart {
         }
         return totalCost;
     }
-    public void print(){
-        for(int i = 0; i <= qtyOrdered; i ++){
-            System.out.println((i + 1) + ". DVD - " + itemsOrdered[i].toString() + ": " + itemsOrdered[i].getCost() + " $");
+
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println(
+                    (i + 1) + ". DVD - " + itemsOrdered[i].toString() + ": " + itemsOrdered[i].getCost() + " $");
         }
-        System.out.println("Total cost: " + totalCost());
-        
+        System.out.println("Total cost: " + totalCost() + "$");
+        System.out.println("***************************************************");
     }
+
+    public DigitalVideoDisc searchByID(int id) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                return itemsOrdered[i];
+            }
+        }
+        return null;
+    }
+
+    public void displaySearchByID(int id) {
+        DigitalVideoDisc disc = searchByID(id);
+        if (disc != null) {
+            System.out.println("DVD found by ID: " + disc.toString());
+        } else {
+            System.out.println("Not found DVD with ID: " + id);
+        }
+    }
+
+    public void displaySearchByTitle(String title) {
+        boolean found = false;
+        System.out.print("DVDs found by Title:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                System.out.println(itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Not found DVD with title: " + title);
+        }
+    }
+
 }
