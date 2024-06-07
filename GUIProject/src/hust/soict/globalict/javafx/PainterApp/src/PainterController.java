920ce86783423da3a420413efc2c322ad8bb0625
Hoang Manh Cuong <khanh.nnq226047@sis.hust.edu.vn>
2024-06-07 22:11:19 +0700

    complte painter

---
+++ GUIProject/src/hust/soict/globalict/javafx/PainterApp/src/PainterController.java
@@ -0,0 +1,37 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class PainterController {
+
+    @FXML
+    private Pane drawingAreaPane;
+
+    @FXML
+    private RadioButton penRadioButton;
+
+    @FXML
+    private RadioButton eraserRadioButton;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+        Color color = Color.BLACK; // Default to pen color
+
+        if (eraserRadioButton.isSelected()) {
+            color = Color.WHITE; // Set color to white for eraser
+        }
+
+        Circle newCircle = new Circle(event.getX(), event.getY(), 4, color);
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+}
